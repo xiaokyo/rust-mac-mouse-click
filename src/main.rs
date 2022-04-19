3b72bfbecad46e5526cc134cd0dfb7906b6d5605
小K <904656778@qq.com>
2022-04-19 17:54:30 +0800

    fix: 优化代码

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,46 @@ use core_graphics::event_source::{CGEventSource, CGEventSourceStateID};
 use core_graphics::geometry::CGPoint;
 use std::env;
 
+// 鼠标按键枚举
+enum MouseEvent {
+    Left,
+    Right,
+}
+
+enum MouseAction {
+    Press,
+    Release,
+}
+
+/**
+ * 发送鼠标事件
+ */
+fn send_mouse_event(_type: MouseEvent, _action: MouseAction, x: f64, y: f64) {
+    let ev_left_down_source = CGEventSource::new(CGEventSourceStateID::HIDSystemState).unwrap();
+
+    let (mut event_type, mouse_button) = match _type {
+        MouseEvent::Left => (CGEventType::LeftMouseDown, CGMouseButton::Left),
+        MouseEvent::Right => (CGEventType::RightMouseDown, CGMouseButton::Right),
+    };
+
+    if let MouseAction::Release = _action {
+        // 如果是松开就替换松开的event_type
+        event_type = match _type {
+            MouseEvent::Left => CGEventType::LeftMouseUp,
+            MouseEvent::Right => CGEventType::RightMouseUp,
+        };
+    }
+
+    let event = CGEvent::new_mouse_event(
+        ev_left_down_source,
+        event_type,
+        CGPoint::new(x, y),
+        mouse_button,
+    )
+    .unwrap();
+    event.post(CGEventTapLocation::HID);
+}
+
 fn main() {
     let arr: Vec<String> = env::args().collect();
     let x = arr.get(1).expect("x is required");
@@ -10,25 +50,6 @@ fn main() {
 
     let y = arr.get(2).expect("y is required").parse::<f64>().unwrap();
 
-    let ev_left_down_source = CGEventSource::new(CGEventSourceStateID::HIDSystemState).unwrap();
-
-    let event = CGEvent::new_mouse_event(
-        ev_left_down_source,
-        CGEventType::LeftMouseDown,
-        CGPoint::new(x, y),
-        CGMouseButton::Left,
-    )
-    .unwrap();
-    event.post(CGEventTapLocation::HID);
-
-    let ev_left_release_source = CGEventSource::new(CGEventSourceStateID::HIDSystemState).unwrap();
-    let event = CGEvent::new_mouse_event(
-        ev_left_release_source,
-        CGEventType::LeftMouseUp,
-        CGPoint::new(x, y),
-        CGMouseButton::Left,
-    )
-    .unwrap();
-
-    event.post(CGEventTapLocation::HID);
+    send_mouse_event(MouseEvent::Left, MouseAction::Press, x, y);
+    send_mouse_event(MouseEvent::Left, MouseAction::Release, x, y);
 }
